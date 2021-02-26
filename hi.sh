14f808f5335931b4d456fe068ede76a0639c2cde
sid2699 <siddhantbokil26@gmail.com>
2021-02-26 09:38:00 +0000

    search email and display shit
    Signed-off-by: sid2699 <siddhantbokil26@gmail.com>

--- hi.sh
+++ hi.sh
@@ -1,1 +1,6 @@
 grep -nr $@ data.txt
+thefile=`grep -nr $@ data.txt`
+rm -rf hue.txt
+echo $thefile > hue.txt
+theemail=`grep -E -o "\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,6}\b" hue.txt`
+echo "Email is: $theemail"
